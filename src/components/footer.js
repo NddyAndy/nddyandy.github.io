a3c4e091a147f2d4201e981d026edb258a3ea522
Ndifreke Friday <iam.ndie@gmail.com>
2019-06-03 15:37:14 +0100

    new design

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+
+        <footer className="container footer">
+            <p className="copy">Made with <span>&hearts;</span> by <a href="https://twitter.com/ndiecodes" target="_blank">ndiecodes</a>.</p>
+        </footer>
+        
+    )
+}
+
+
+export default Footer;
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+
+        <div className="container">
+            <h1 className="heading">
+                <span className="heading-primary"> <span className='hi'>Hi</span><span className="dot"></span></span>
+
+                <span className="heading-secondary">I'm Ndifreke Friday,</span>
+                <span className="heading-tertiary">A web developer based out of Port Harcourt, Nigeria. I build responsive websites and scalable APIs. I am problem solving and always excited when it comes to mentoring aspiring developers.</span>
+            </h1>
+
+
+            <div className="blog">
+
+                <h2>Recent  Blog Posts</h2>
+
+                <div className="blog-card">
+                    
+
+                </div>
+
+            </div>
+
+            
+        </div>
+
+    
+
+
+    </Layout>
+)
