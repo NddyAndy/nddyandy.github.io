c19207496b758ecdfd150b2e1743aa20604f08db
Ndifreke Friday <iam.ndie@gmail.com>
2019-06-04 09:31:52 +0100

    checkpoint

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
 
         <footer className="container footer">
-            <p className="copy">Made with <span>&hearts;</span> by <a href="https://twitter.com/ndiecodes" target="_blank">ndiecodes</a>.</p>
+            <p className="copy">Made with <span>&hearts;</span> by <a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">ndiecodes</a>.</p>
         </footer>
         
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,76 @@ export default () => (
                 <span className="heading-tertiary">A web developer based out of Port Harcourt, Nigeria. I build responsive websites and scalable APIs. I am problem solving and always excited when it comes to mentoring aspiring developers.</span>
             </h1>
 
+            {/* <hr /> */}
 
-            <div className="blog">
 
-                <h2>Recent  Blog Posts</h2>
+            <div className="portfolio-area">
+ 
+                <h2 className="heading-main">Projects</h2>
+                <div className="porfolio">
 
-                <div className="blog-card">
+                    <div className="portfolio-card">
+                        <h2><a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">Teachable.ng</a></h2>
+                        <p className="tech"><em> <span>Tech Used: </span> HTML/CSS, Javascript(Nodejs)</em></p>
+
+                        <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+
+                    </div>
+
+                     <div className="portfolio-card">
+                        <h2><a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">nbte.forum.org.ng/ranking</a></h2>
+                        <p className="tech"><em> <span>Tech Used: </span> HTML/CSS, Javascript(Nodejs)</em></p>
+
+                        <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+
+                    </div>
+
+
+                     <div className="portfolio-card">
+                        <h2><a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">MLM Solution</a></h2>
+                        <p className="tech"><em> <span>Tech Used: </span> HTML/CSS, Javascript(Nodejs)</em></p>
+
+                        <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not </p>
+
+                    </div>
+
+                     <div className="portfolio-card">
+                        <h2><a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">Optisoft.ng</a></h2>
+                        <p className="tech"><em> <span>Tech Used: </span> HTML/CSS, Javascript, PHP(Laravel)</em></p>
+
+                        <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not </p>
+
+                    </div>
+
+                    <div className="portfolio-card">
+                        <h2><a href="https://twitter.com/ndiecodes" target="_blank" rel="noopener noreferrer">nddyacademy.herokuapp.com</a></h2>
+                        <p className="tech"><em> <span>Tech Used: </span> HTML/CSS, Javascript, PHP(Laravel)</em></p>
+
+                        <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not </p>
+
+                    </div>
+
                     
 
                 </div>
+               
 
             </div>
 
+            <div className="Skills">
+
+                <h2 className="heading-main">My Skills</h2>
+                
+
+            </div>
+
+
+               <div className="Skills">
+
+                    <h2 className="heading-main">My Skills</h2>
+
+
+                </div>
             
         </div>
 
