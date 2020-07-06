2a1a660b05a8b287bd65656c42371a1817b9fc26
Ndifreke Friday <ndiecodes@gmail.com>
2020-07-06 13:54:34 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,7 @@ export default () => (
         <span className="heading-tertiary">
           My core skills are <b>HTML</b>, <b>CSS</b> (SASS), <b>Javascript</b>{" "}
           (React, Vue, <b>Nodejs</b>) and <b>PHP</b>(Laravel). I also have
-          experience with DBMS [ <b>Mysql</b> / NoSql (<b>mongoDB</b> and
-          firebase)], <b>git</b> version control and <b>Linux</b>.
+          experience with DBMS [ <b>Mysql</b> / NoSql (<b>mongoDB</b>)], <b>git</b> version control and <b>Linux</b>.
         </span>
         <span className="heading-tertiary">
           I have completed <Link to="projects">projects</Link> alone and also
